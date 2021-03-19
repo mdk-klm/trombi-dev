7747b9a24a8007e58bad44c2439cd4633e69ede7
mdk-klm <antoine.04@laposte.net>
2021-03-19 09:43:58 +0100

    modify birthdate

--- App.js
+++ App.js
@@ -3,9 +3,16 @@ const express = require("express");
 const app = express();
 
 
-app.use(express.json());
 
-const readUsers = () => JSON.parse(fs.readFileSync("./user.json").toString());
+
+const getAge = birthDate => new Date(
+  (Date.now() - Date.parse(birthDate))
+).getFullYear() - 1970
+
+const readUsers = () => JSON.parse(fs.readFileSync("./user.json").toString()).map(user => ({
+  ...user,
+  age: getAge(user.birthDate)
+}));
 
 app.get("/users", (req, res) => {
   res.json(readUsers());
@@ -16,18 +23,10 @@ app.post("/users", (req, res) => {
   // Récupère la liste des users
   const users = readUsers();
 
-  const emailUsers = users;
-  let isEmailUsed = 0;
-  emailUsers
-    .filter((user) => user.id != id)
-    .forEach((user) => {
-      if (user.email === body.email) {
-        console.log("Cet email est déjà utilisé.");
-        isEmailUsed = -1;
-      }
-    });
-  if (isEmailUsed === -1)
-    return res.json({ errorMail: "Cet email est déjà utilisé" });
+  if(users.filter((user) => user.email === body.email)){
+    return res.json({messageError: 'cet email est déja utilisé'})
+  } else {
+  
   // Création du nouveau user
   const newUser = {
     id: Math.max(...users.map((user) => user.id)) + 1,
@@ -40,29 +39,22 @@ app.post("/users", (req, res) => {
   };
   // Ajoute le nouveau user dans le tableau d'users
   users.push(newUser);
-  // Ecris dans le fichier pour insérer la liste des users
-  fs.writeFileSync("./user.json", JSON.stringify(users, null, 4));
-  res.json(users);
-});
+  if (users.filter((user) => user.id !== newUser.id), users.some((user) => user.email === newUser.email) ) {
+    return res.json({messageError: "Email déjà utilisé"});
+  } else {
+    // Ajoute le nouveau user dans le tableau d'users
+    const newUsers = [...users.filter((user) => user.id !== id), newUser];
+    // Ecris dans le fichier pour insérer la liste des users
+    fs.writeFileSync("./user.json", JSON.stringify(newUsers, null, 4));
+    res.json(newUser);
+  }
+}});
 
 app.put("/users/:id", (req, res) => {
   const body = req.body;
 
   // Récupère la liste des users
   const users = readUsers();
-
-  const emailUsers = users;
-  let isEmailUsed = 0;
-  emailUsers
-    .filter((user) => user.id != id)
-    .forEach((user) => {
-      if (user.email === body.email) {
-        console.log("Cet email est déjà utilisé.");
-        isEmailUsed = -1;
-      }
-    });
-  if (isEmailUsed === -1)
-    return res.json({ errorMail: "Cet email est déjà utilisé" });
 
   // Création du nouveau user
 
@@ -76,11 +68,16 @@ app.put("/users/:id", (req, res) => {
     avatarUrl: body.avatarUrl,
     gender: body.gender,
   };
-  // Ajoute le nouveau user dans le tableau d'users
-  const newUsers = [...users.filter((user) => user.id !== id), newUser];
-  // Ecris dans le fichier pour insérer la liste des users
-  fs.writeFileSync("./user.json", JSON.stringify(newUsers, null, 4));
-  res.json(newUser);
+
+  if (users.filter((user) => user.id !== newUser.id), users.some((user) => user.email === newUser.email) ) {
+    return res.json({messageError: "Email déjà utilisé"});
+  } else {
+    // Ajoute le nouveau user dans le tableau d'users
+    const newUsers = [...users.filter((user) => user.id !== id), newUser];
+    // Ecris dans le fichier pour insérer la liste des users
+    fs.writeFileSync("./user.json", JSON.stringify(newUsers, null, 4));
+    res.json(newUser);
+  }
 });
 
 app.get("/users/:id", (req, res) => {
@@ -93,20 +90,26 @@ app.get("/users/:id", (req, res) => {
   res.json(user);
 });
 
-app.delete("/user/:id", (req, res) => {
-  //on récupère l'id du user a supprimer et la liste des users
-  const id = req.params.id;
+app.delete("/users/:id", (req, res) => {
+
+  // Récupère la liste des users
   const users = readUsers();
 
-  const NewUsers = users.filter((user) => user.id != id);
-  fs.writeFileSync("./user.json", JSON.stringify(NewUsers, null, 4));
-  res.json(NewUsers);
+  // Création du nouveau user
+
+  const id = Number(req.params.id);
+
+  // supprime le user dans le tableau d'users
+  const deleteUser = users.filter((user) => user.id !== id);
+  // Ecris dans le fichier pour insérer la liste des users
+  fs.writeFileSync("./user.json", JSON.stringify(deleteUser, null, 4));
+  res.json(deleteUser);
 });
 
-//fonction pour l'upload d'image
-app.post("/upload/:id", (req, res) => {
-  const id = req.params.id;
-  let path = "";
-});
+app.post("/users/:id/photo", (req, res) => {
+  const photoLocation = req.files.photo.mv('./public/fff.png');
+
+  // reutiliser le put pour modifier le user et mettre le path de la photo dans avatarUrl
+})
 
 app.listen(8081, () => console.log("server is running"));
